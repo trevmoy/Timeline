0184da5c8ef7f735cac8e39e18e8521c05b7657d
Trevor Moy <51927676+trevmoy@users.noreply.github.com>
2024-01-10 14:01:00 -0500

    Updated timeline.py, tl.sh

--- timeline.py
+++ timeline.py
@@ -24,4 +24,5 @@ def get_keyword(num) -> str:
     This function returns the content of a cell in the keyword column according to the num parameter
     """
     return str(df.at[num, 'keyword'])
+
     
--- tl.sh
+++ tl.sh
@@ -9,35 +9,35 @@ outputfile="timeline.md"
 echo "\`\`\`mermaid\ntimeline\n\ttitle CSC 392 Course Timeline\n" >> $outputfile
 # for loop loops through each class of the semester
 
-for i in {1..30}
+for i in {0..29}
 do
-    # If else loop for determining which week should be echoed
-    if [ $(($i)) -eq 1 ];
+    # If else for determining which week should be output
+    # This if else condition uses j which is the for loops iteration + 1 so that we get the correct number of 
+    j=$(($j + 1))
+    if [ $(($j % 2)) -eq 0 ] && [ $(($j)) != 1 ] && [ $(($j)) != 2 ];
     then
-        echo "\t\tsection Week 1" >> $outputfile
-    elif [ $(($i % 2)) -eq 0 ] && [ $(($i)) != 1 ];
-    then
-        currweek=$(($i / 2))
+        currweek=$(($j / 2))
+        echo "$currweek"
         echo "\t\tsection Week $currweek" >> $outputfile
     fi
 
-    # checking for the special case where the remainder is 1
-    if [ $(($i)) -eq 1 ];
+    # checking for the special case where the remainder is 1 (in terms of j)
+    if [ $(($j)) -eq 1 ];
     then
-        date1=$(python3 -c "import timeline; print(timeline.get_date(1))")
-        question1=$(python3 -c "import timeline; print(timeline.get_question(1))")
-        keyword1=$(python3 -c "import timeline; print(timeline.get_keyword(1))")
+        date1=$(python3 -c "import timeline; print(timeline.get_date($i))")
+        question1=$(python3 -c "import timeline; print(timeline.get_question($i))")
+        keyword1=$(python3 -c "import timeline; print(timeline.get_keyword($i))")
+        echo "\t\tsection Week 1" >> $outputfile
         echo "\t\t$date1 - $keyword1 - $question1" >> $outputfile
     # checks for even
-    elif [ $(($i % 2)) -eq 0 ] && [ $(($i)) != 1 ];
+    elif [ $(($j % 2)) -eq 0 ];
     then
     # commands call the specified function from the python script 
         date2=$(python3 -c "import timeline; print(timeline.get_date($i))")
         question2=$(python3 -c "import timeline; print(timeline.get_question($i))")
         keyword2=$(python3 -c "import timeline; print(timeline.get_keyword($i))")
         echo "\t\t$date2 - $keyword2 - $question2" >> $outputfile
-    elif [ $(($i % 3)) -eq 0 ] && [ $(($i)) != 1 ];
-    then
+    else
     # checking if the remainder is any other odd number
         # commands run the functions for the 2nd day of the week
         date3=$(python3 -c "import timeline; print(timeline.get_date($i))")
