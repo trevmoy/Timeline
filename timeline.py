cb8eb9ba62f28f82995fff35aecb7af788f57f96
Trevor Moy <51927676+trevmoy@users.noreply.github.com>
2024-01-23 15:56:29 -0500

    Added labschedule.csv, schedule.csv, timeline.py, tl.sh

--- timeline.py
+++ timeline.py
@@ -3,26 +3,50 @@ Here we will test pandas dataframes with running a script to make a mermaid time
 """
 import pandas as pd
 
-df = pd.read_csv('schedule.csv',index_col='date').sort_index()
+# Dataframe 1 for the normal class schedule
+df1 = pd.read_csv('schedule.csv',index_col='date').sort_index()
+# Dataframe 2 for the normal class schedule
+df2 = pd.read_csv('labschedule.csv',index_col='date').sort_index()
 
-df = df.reset_index()
+df1 = df1.reset_index()
+df2 = df2.reset_index()
+# For df1
 
-def get_date(num) -> str:
+def get_df1_date(num) -> str:
     """
     This function returns the content of a cell in the date column according to the num parameter
     """
-    return str(df.at[num, 'date'])
+    return str(df1.at[num, 'date'])
 
-def get_question(num) -> str:
+def get_df1_question(num) -> str:
     """
     This function returns the content of a cell in the question column according to the num parameter
     """
-    return str(df.at[num, 'question'])
+    return str(df1.at[num, 'question'])
 
-def get_keyword(num) -> str:
+def get_df1_keyword(num) -> str:
     """
     This function returns the content of a cell in the keyword column according to the num parameter
     """
-    return str(df.at[num, 'keyword'])
+    return str(df1.at[num, 'keyword'])
 
+# For df2
+
+def get_df2_date(num) -> str:
+    """
+    This function returns the content of a cell in the date column according to the num parameter
+    """
+    return str(df2.at[num, 'date'])
+
+def get_df2_topic(num) -> str:
+    """
+    This function returns the content of a cell in the topic column according to the num parameter
+    """
+    return str(df2.at[num, 'topic'])
+
+def get_df2_activity(num) -> str:
+    """
+    This function returns the content of a cell in the activity column according to the num parameter
+    """
+    return str(df2.at[num, 'activity'])
     
--- tl.sh
+++ tl.sh
@@ -14,37 +14,50 @@ do
     # If else for determining which week should be output
     # This if else condition uses j which is the for loops iteration + 1 so that we get the correct number of 
     j=$(($j + 1))
-    if [ $(($j % 2)) -eq 0 ] && [ $(($j)) != 1 ] && [ $(($j)) != 2 ];
-    then
-        currweek=$(($j / 2))
-        echo "\t\tsection Week $currweek" >> $outputfile
-    fi
-
     # checking for the special case where the remainder is 1 (in terms of j)
     if [ $(($j)) -eq 1 ];
     then
-        date1=$(python3 -c "import timeline; print(timeline.get_date($i))")
-        question1=$(python3 -c "import timeline; print(timeline.get_question($i))")
-        keyword1=$(python3 -c "import timeline; print(timeline.get_keyword($i))")
+        # For lecture
+        lecturedate1=$(python3 -c "import timeline; print(timeline.get_df1_date($i))")
+        question1=$(python3 -c "import timeline; print(timeline.get_df1_question($i))")
+        keyword1=$(python3 -c "import timeline; print(timeline.get_df1_keyword($i))")
+        
+
         echo "\t\tsection Week 1" >> $outputfile
-        echo "\t\t$date1 - $keyword1 - $question1" >> $outputfile
+        echo "\t\t$lecturedate1 - $keyword1 - $question1" >> $outputfile
+
     elif [ $(($j % 2)) -eq 0 ];
     # checks for even
     then
-        date2=$(python3 -c "import timeline; print(timeline.get_date($i))")
-        question2=$(python3 -c "import timeline; print(timeline.get_question($i))")
-        keyword2=$(python3 -c "import timeline; print(timeline.get_keyword($i))")
-        echo "\t\t$date2 - $keyword2 - $question2" >> $outputfile
+        # Gets current week
+        currweek=$(($j / 2))
     
+        lecturedate2=$(python3 -c "import timeline; print(timeline.get_df1_date($i))")
+        question2=$(python3 -c "import timeline; print(timeline.get_df1_question($i))")
+        keyword2=$(python3 -c "import timeline; print(timeline.get_df1_keyword($i))")
+
+        # For lab
+        labdate1=$(python3 -c "import timeline; print(timeline.get_df2_date($currweek))")
+        topic1=$(python3 -c "import timeline; print(timeline.get_df2_topic($currweek))")
+        activity1=$(python3 -c "import timeline; print(timeline.get_df2_activity($currweek))")
+
+        echo "\t\t$lecturedate2 - $keyword2 - $question2" >> $outputfile
+        echo "\t\t$labdate1 - $topic1 - $activity1" >> $outputfile
+        
+        # Outputs the current week as a section
+        echo "\t\tsection Week $currweek" >> $outputfile
     else
         # checking if the remainder is any other odd number
         # commands run the functions for the 2nd day of the week
         # does not count 3 because it messes the output up
-        date3=$(python3 -c "import timeline; print(timeline.get_date($i))")
-        question3=$(python3 -c "import timeline; print(timeline.get_question($i))")
-        keyword3=$(python3 -c "import timeline; print(timeline.get_keyword($i))")
-        echo "\t\t$date3 - $keyword3 - $question3" >> $outputfile
+
+        lecturedate3=$(python3 -c "import timeline; print(timeline.get_df1_date($i))")
+        question3=$(python3 -c "import timeline; print(timeline.get_df1_question($i))")
+        keyword3=$(python3 -c "import timeline; print(timeline.get_df1_keyword($i))")
+
+        echo "\t\t$lecturedate3 - $keyword3 - $question3" >> $outputfile
     fi
+    
 
 done
 echo "\n\`\`\`" >> $outputfile
